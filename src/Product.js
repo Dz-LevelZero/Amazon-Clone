a7aeeadd1ed2c965460d68fd4c53d8eb68d992ea
Sami <01@aa.aa>
2020-09-11 08:23:12 +0100

    6- Using React Context Api

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import "./Product.css"
 import TextTruncate from "react-text-truncate";
+import { useStateValue } from './StateProvider';
 
 function Product({id,title, image, price, rating}) {
+    const [{ basket }, dispatch] = useStateValue();
+    console.log("this is the basket >>>>>",basket);  
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch ({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -22,7 +39,7 @@ function Product({id,title, image, price, rating}) {
                 src={image} 
                 alt=""
             />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
 
             
         </div>
